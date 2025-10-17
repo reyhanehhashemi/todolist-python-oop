987e898a607e2c4bd074b4a1f3e530b095b45e8b
Reyhaneh Hajihashemi <reyhanehhashemi171@gmail.com>
2025-10-17 11:11:38 +0330

    feat: implement ProjectRepository with in-memory storage

--- src/todolist/repositories/__init__.py
+++ src/todolist/repositories/__init__.py
@@ -1,5 +1,6 @@
 """Repositories package."""
 
 from .task_repository import TaskRepository
+from .project_repository import ProjectRepository
 
-__all__ = ["TaskRepository"]
+__all__ = ["TaskRepository", "ProjectRepository"]
